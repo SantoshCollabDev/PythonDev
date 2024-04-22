15eff461963da3de2a79a14eac4a86d24251f1c0
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-04-22 10:08:58 +0530

    udpate for chances left

--- Basic/randint.py
+++ Basic/randint.py
@@ -1,9 +1,9 @@
 import random
-rand_gen_num = random.randint(1,100)
+rand_gen_num = random.randint(1,10)
 print(rand_gen_num)
 
 user_won = False
-for chance in range(1,7):
+for chance in range(6, 1, -1):
   user_guess_num = int(input("Enter a number between 1 to 100"))
   if user_guess_num < rand_gen_num:
     print("Your number < number to be guessed")
@@ -13,6 +13,7 @@ for chance in range(1,7):
     print("Awesome !! You have WON")
     user_won = True
     break
-
+  print(f"You have {chance - 2} chances more left")
+  
 if not user_won:
   print(" Better Luck Next Time!")
