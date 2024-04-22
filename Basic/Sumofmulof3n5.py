7b88b6fc1acd15c1f3288ec4bf76410521106be5
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-04-22 07:49:13 +0530

    Update index variable

--- Basic/Sumofmulof3n5.py
+++ Basic/Sumofmulof3n5.py
@@ -2,12 +2,12 @@
 Find the sum of all the multiples of 3 or 5 below 1000
 '''
 
-limit_number = 1000
+index = 1000
 
 sum = 0
 digit = 1
 
-while digit < limit_number:
+while digit < index:
   if digit%3 == 0 or digit%5 == 0:
     sum += digit
   digit+=1
