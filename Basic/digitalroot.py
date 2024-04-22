bd1479bfcd8e4df1430185725f444d03b712a576
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-04-22 08:25:21 +0530

    New: Digit Root Program

---
+++ Basic/digitalroot.py
@@ -0,0 +1,24 @@
+'''
+Digital Root of 1729 ==>
+
+1 + 7 + 2 + 9 = 19
+since 19 is a 2 digit number repeat the digit sum process
+
+1 + 9 = 10
+since 10 is a 2 digit number repeat the digit sum process
+
+1+ 0 = 1
+
+Since result is a single digit number i.e. < 10 STOP the process
+'''
+
+number = int(input("enter a number"))
+
+while number > 9:
+  sum = 0
+  while number > 0:
+    sum += number%10
+    number = number//10
+  number = sum
+
+print(sum)
