3962425530ce1fb4676de674dd251606da3ef274
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-05-05 09:43:27 +0530

    inheritance

--- Projects/oop_demo/learning/accounts/account.py
+++ Projects/oop_demo/learning/accounts/account.py
@@ -29,4 +29,28 @@ class BankAccount:
        """
        self.balance += amount
     
+class SavingsAccount(BankAccount):
+   """ This class represents the savings account structure
+   """
+
+   # Note: we dont need to repeat method as it is already 
+   # happening at parent class
    
+   def withdraw(self, amount):
+      """ This method is overriden to implement non-negative balanaces
+      """
+      if self.balance - amount < 0:
+         # dont allow withdrawl
+         # fail this
+         print("This transction is not allowed")
+         return 
+      return super().withdraw(amount)
+
+   def deposit(self, amount):
+      """
+      This method stops user from depositing > 10 lakhs
+      """
+      if amount >     1000000:
+         print("Not allowed")
+         return
+      return super().deposit(amount)
